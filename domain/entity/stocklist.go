915165784389123e7f57aa63d4789e27a15a60f9
digeon-ito <s-ito@digeon.co>
2023-06-15 22:17:29 +0900

    fix bug

--- domain/entity/stocklist.go
+++ domain/entity/stocklist.go
@@ -8,7 +8,7 @@ type StockList struct {
 // Stocks
 // 株式分割情報を反映した株価
 func (s *StockList) Stocks() []*Stock {
-	var ret []*Stock
+	ret := make([]*Stock, len(s.StockList))
 	copy(ret, s.StockList)
 	for _, split := range s.StockSplits {
 		for _, stock := range ret {
